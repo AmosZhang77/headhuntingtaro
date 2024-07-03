93f4d741adcf8247f8059785e2ecbd57320a2e72
AmosZhang <moonyellow@126.com>
2024-07-03 19:25:52 +0800

    taro+openapi-typescript

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from '@tarojs/components'
+import { useLoad } from '@tarojs/taro'
+import './index.less'
+
+export default function Index() {
+
+  useLoad(() => {
+    console.log('Page loaded.')
+  })
+
+  return (
+    <View className='index'>
+      <Text>Hello world!</Text>
+      <Text>1111</Text>
+
+    </View>
+  )
+}
